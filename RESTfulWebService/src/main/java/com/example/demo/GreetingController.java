d03d9504f9eb308954f363aef5d1605a6fa615fd
SirishaVemparala-Wipro <sirishavemparala1203@gmail.com>
2025-02-11 14:39:38 +0530

    Day 9

--- RESTfulWebService/src/main/java/com/example/demo/GreetingController.java
+++ RESTfulWebService/src/main/java/com/example/demo/GreetingController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
  * the controller will ensure that the class will control how the object will be
  * used it is done using 2 parts. 1.get mapping 2.request param
  **/
+//Marks the class as a RESTful web controller that returns JSON responses.
 @RestController
 public class GreetingController {
 	/*
@@ -19,7 +20,10 @@ public class GreetingController {
 	private static final String template = "Hello, %s!";
 	private final AtomicLong atomicLong = new AtomicLong();// atomic long is a datatype that will handle huge data
 
-	@GetMapping("/greeting")
+	@GetMapping("/greeting")//Maps HTTP GET requests to a method.
+	//RequestParam Extracts query parameters from the request.
+	//atomicLong.incrementAndGet(): Increments the counter and returns the new value.
+	//String.format(template, name): Fills the %s placeholder with the name provided by the user.
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(atomicLong.incrementAndGet(), String.format(template, name));
 	}
---
+++ SBA/src/main/java/com/example/demo/Tomcat.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+
+
+public class Tomcat {
+	
+	public String tomcatCheck() {
+		return "Tomcat is Running";
+	}
+	
+	
+
+}
